21fb04e6cfec2bcdcbd38fafb5ba4b2338e9e62a
karusb <sokucam@hotmail.com>
2018-07-11 00:24:07 +0300

    Add project files.

---
+++ ShutdownInTime/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace ShutdownInTime
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.InputBox = new System.Windows.Forms.TextBox();
+            this.DKradio = new System.Windows.Forms.RadioButton();
+            this.SaatRadio = new System.Windows.Forms.RadioButton();
+            this.SaatDKradio = new System.Windows.Forms.RadioButton();
+            this.RemainingLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(30, 102);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 23);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(180, 102);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(75, 23);
+            this.StopButton.TabIndex = 1;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // InputBox
+            // 
+            this.InputBox.Location = new System.Drawing.Point(90, 62);
+            this.InputBox.Name = "InputBox";
+            this.InputBox.Size = new System.Drawing.Size(100, 20);
+            this.InputBox.TabIndex = 2;
+            // 
+            // DKradio
+            // 
+            this.DKradio.AutoSize = true;
+            this.DKradio.Location = new System.Drawing.Point(30, 29);
+            this.DKradio.Name = "DKradio";
+            this.DKradio.Size = new System.Drawing.Size(59, 17);
+            this.DKradio.TabIndex = 3;
+            this.DKradio.TabStop = true;
+            this.DKradio.Text = "Dakika";
+            this.DKradio.UseVisualStyleBackColor = true;
+            this.DKradio.CheckedChanged += new System.EventHandler(this.DKradio_CheckedChanged);
+            // 
+            // SaatRadio
+            // 
+            this.SaatRadio.AutoSize = true;
+            this.SaatRadio.Location = new System.Drawing.Point(121, 29);
+            this.SaatRadio.Name = "SaatRadio";
+            this.SaatRadio.Size = new System.Drawing.Size(47, 17);
+            this.SaatRadio.TabIndex = 4;
+            this.SaatRadio.TabStop = true;
+            this.SaatRadio.Text = "Saat";
+            this.SaatRadio.UseVisualStyleBackColor = true;
+            this.SaatRadio.CheckedChanged += new System.EventHandler(this.SaatRadio_CheckedChanged);
+            // 
+            // SaatDKradio
+            // 
+            this.SaatDKradio.AutoSize = true;
+            this.SaatDKradio.Location = new System.Drawing.Point(212, 29);
+            this.SaatDKradio.Name = "SaatDKradio";
+            this.SaatDKradio.Size = new System.Drawing.Size(84, 17);
+            this.SaatDKradio.TabIndex = 5;
+            this.SaatDKradio.TabStop = true;
+            this.SaatDKradio.Text = "Saat:Dakika";
+            this.SaatDKradio.UseVisualStyleBackColor = true;
+            this.SaatDKradio.CheckedChanged += new System.EventHandler(this.SaatDKradio_CheckedChanged);
+            // 
+            // RemainingLabel
+            // 
+            this.RemainingLabel.AutoSize = true;
+            this.RemainingLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RemainingLabel.Location = new System.Drawing.Point(12, 128);
+            this.RemainingLabel.Name = "RemainingLabel";
+            this.RemainingLabel.Size = new System.Drawing.Size(212, 73);
+            this.RemainingLabel.TabIndex = 6;
+            this.RemainingLabel.Text = "00000";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(314, 207);
+            this.Controls.Add(this.RemainingLabel);
+            this.Controls.Add(this.SaatDKradio);
+            this.Controls.Add(this.SaatRadio);
+            this.Controls.Add(this.DKradio);
+            this.Controls.Add(this.InputBox);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.StartButton);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Doğru Zamanda PC\'yi Kapat";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.TextBox InputBox;
+        private System.Windows.Forms.RadioButton DKradio;
+        private System.Windows.Forms.RadioButton SaatRadio;
+        private System.Windows.Forms.RadioButton SaatDKradio;
+        private System.Windows.Forms.Label RemainingLabel;
+    }
+}
+
---
+++ ShutdownInTime/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShutdownInTime
+{
+    public partial class Form1 : Form
+    {
+        int selection = 0;
+        int seconds = 0;
+        Timer timer;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DKradio_CheckedChanged(object sender, EventArgs e)
+        {
+            selection = 1;
+        }
+
+        private void SaatRadio_CheckedChanged(object sender, EventArgs e)
+        {
+            selection = 2;
+        }
+
+        private void SaatDKradio_CheckedChanged(object sender, EventArgs e)
+        {
+            selection = 3;
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+
+            timer = new Timer();
+            timer.Interval = (1000);
+
+
+            string cmdtext = InputBox.Text;
+            string[] bothtext = new string[2];
+            int mins = 0;
+            int hours = 0;
+
+            if(cmdtext.Contains(":") && selection == 3)
+            {
+                bothtext = cmdtext.Split(':');
+                hours= int.Parse(bothtext[0]);
+                mins = int.Parse(bothtext[1]);
+                seconds = (hours * 60 * 60) + (mins * 60);
+            }
+            else if (selection == 2)
+            {
+                hours = int.Parse(cmdtext);
+                seconds = (hours * 60 * 60);
+            }
+            else if(selection == 1)
+            {
+                mins = int.Parse(cmdtext);
+                seconds = (mins * 60);
+            }
+            //RemainingLabel.Text = hours.ToString() + " : " + mins.ToString();
+            string command = "shutdown /s /f /t " + seconds.ToString();
+            ExecuteCommandSync(command);
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Start();
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            ExecuteCommandSync("shutdown /a");
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            seconds -= 1;
+            RemainingLabel.Text = seconds.ToString() ;
+            Application.DoEvents();
+        }
+        /// <span class="code-SummaryComment"><summary></span>
+        /// Executes a shell command synchronously.
+        /// <span class="code-SummaryComment"></summary></span>
+        /// <span class="code-SummaryComment"><param name="command">string command</param></span>
+        /// <span class="code-SummaryComment"><returns>string, as output of the command.</returns></span>
+        public void ExecuteCommandSync(object command)
+        {
+            try
+            {
+                // create the ProcessStartInfo using "cmd" as the program to be run,
+                // and "/c " as the parameters.
+                // Incidentally, /c tells cmd that we want it to execute the command that follows,
+                // and then exit.
+                System.Diagnostics.ProcessStartInfo procStartInfo =
+                    new System.Diagnostics.ProcessStartInfo("cmd.exe", "/c " + command);
+
+                // The following commands are needed to redirect the standard output.
+                // This means that it will be redirected to the Process.StandardOutput StreamReader.
+                procStartInfo.RedirectStandardOutput = true;
+                procStartInfo.UseShellExecute = false;
+                // Do not create the black window.
+                procStartInfo.CreateNoWindow = true;
+                // Now we create a process, assign its ProcessStartInfo and start it
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                proc.StartInfo = procStartInfo;
+                proc.Start();
+                // Get the output into a string
+                string result = proc.StandardOutput.ReadToEnd();
+                // Display the command output.
+                Console.WriteLine(result);
+            }
+            catch (Exception objException)
+            {
+                // Log the exception
+            }
+        }
+    }
+}
